d230e3082207a98cfb51a5f989784900555ad1db
Gaeun Lee <gaeun0800@gmail.com>
2022-07-03 21:53:04 +0900

    인포윈도우 붙여넣기

--- CSS&BT/script.js
+++ CSS&BT/script.js
@@ -67,17 +67,28 @@ const dataSet = [
 // 비동기 처리로 구현
 var geocoder = new kakao.maps.services.Geocoder();
 
-async function setMap(){
+// async function setMap(){
 
-	for (var i = 0; i < dataSet.length; i ++) {
-		// 마커 생성
-		let coords=await getCoordsByAddress(dataSet[i].address);
-		var marker = new kakao.maps.Marker({
-		 map: map, // 마커를 표시할 지도
-		 position: coords, // 마커를 표시할 위치 (좌표로 변환된 coords 집어넣는다)
-	 });	 
- }
-}
+// 	for (var i = 0; i < dataSet.length; i ++) {
+// 		// 마커 생성
+// 		let coords=await getCoordsByAddress(dataSet[i].address);
+// 		var marker = new kakao.maps.Marker({
+// 		 map: map, // 마커를 표시할 지도
+// 		 position: coords, // 마커를 표시할 위치 (좌표로 변환된 coords 집어넣는다)
+// 	 });	 
+// 	   // 마커에 표시할 인포윈도우를 생성합니다 
+// 	   var infowindow = new kakao.maps.InfoWindow({
+//         content: positions[i].content // 인포윈도우에 표시할 내용
+//     });
+
+//     // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+//     // 이벤트 리스너로는 클로저를 만들어 등록합니다 
+//     // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+//     kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+//     kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+// }
+//  }
+
 
 
 // 주소 -> 좌표 변환 함수
@@ -99,3 +110,52 @@ function getCoordsByAddress(address){
  	}
 
 setMap();
+
+
+
+function getContent(data){
+	// 인포윈도우를 만들어서 리턴한다 ()
+	return `<div>hello</div>`;
+}
+
+
+// 마커에 인포윈도우 붙이기
+
+async function setMap(){
+
+	for (var i = 0; i < dataSet.length; i ++) {
+		// 마커 생성
+		let coords=await getCoordsByAddress(dataSet[i].address);
+		var marker = new kakao.maps.Marker({
+		 map: map, // 마커를 표시할 지도
+		 position: coords, // 마커를 표시할 위치 (좌표로 변환된 coords 집어넣는다)
+	 });	 
+
+
+	  // 마커에 표시할 인포윈도우를 생성합니다 
+	  var infowindow = new kakao.maps.InfoWindow({
+        content: getContent(dataSet[i]) // 인포윈도우에 표시할 내용
+    });
+
+    // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+    // 이벤트 리스너로는 클로저를 만들어 등록합니다 
+    // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+    kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+    kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+}
+
+	// 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+	function makeOverListener(map, marker, infowindow) {
+     return function() {
+        infowindow.open(map, marker);
+    };
+}
+
+	// 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+	function makeOutListener(infowindow) {
+     return function() {
+        infowindow.close();
+    };
+}
+}
+ 
