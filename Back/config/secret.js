d517a8bd68125e098f081a4963488ccf681744b8
Gaeun Lee <gaeun0800@gmail.com>
2022-07-09 23:07:28 +0900

    gitignore 수정

--- Back/config/secret.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    jwtsecret: "jwt_secret_key_9321",
-    host: "3.37.21.87",
-    user: "root",
-    port: "3306",
-    password: "qweasd123@", 
-    database: "FoodMap", // 스키마
-};
